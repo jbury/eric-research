212519664d6225229c3d87f178ce1ddc8542a784
Jason Bury <jruby@amazdong.com>
2014-10-01 00:20:32 -0700

    Fixed instructions and added dist. formula

--- data-analysis.py
+++ data-analysis.py
@@ -2,22 +2,24 @@ import sys
 import csv
 import math
 
+#TODO delete
 print ("the number of arguments: ", len(sys.argv))
 print ("The arguments are: ", str(sys.argv))
 
-Default = 'C:\\\\Users\\\\Eric\\\\Desktop\\\\eric-research\\\\30sec.csv'
-
 videoLengthSeconds = int(input("Enter the length of the video in seconds: "))
-if(len(sys.argv) == 2):
+if(len(sys.argv) < 2):
 #if(len(sys.argv)!=2):
-    print ("Error: No .csv file provided")
+    print ("Error: No .csv file provided.  Exiting.")
+    print ("Seriously, just drag your .csv file over this script.  EZ PZ")
     input()
     sys.exit()
+
+file = sys.argv[1]
 #with open(sys.argv[1], 'r') as f:
 #Get the number of lines in the file
-fileSize = sum(1 for line in open(Default, 'r'))
+fileSize = sum(1 for line in open(file, 'r'))
 
-with open(Default, 'r') as f:
+with open(file, 'r') as f:
     myReader = csv.reader(f, dialect='excel')
 
     frameCount = fileSize - 1
@@ -41,4 +43,9 @@ x = input("YAY")
 
 
 def cosineRule(leg1, leg2, hypot):
-    return math.acos((leg1**2 + leg2**2 - hypot**2)/(2*leg1*leg2))
+    #Do I need all these ()'s?  Probably not, but they're here anyway.
+    return math.acos(((leg1**2) + (leg2**2) - (hypot**2))/(2*leg1*leg2))
+
+#Two tuples go in, the distance between them comes out-You can't explain that!
+def distance(p0, p1):
+    return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
